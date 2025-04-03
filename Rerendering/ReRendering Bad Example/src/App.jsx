fe600993d502d897b40319957c655f3d3d028d03
Sushank Gurung <changteezy>
2025-04-03 14:10:56 +0545

    update

--- Rerendering/ReRendering Bad Example/src/App.jsx
+++ Rerendering/ReRendering Bad Example/src/App.jsx
@@ -1,23 +1,45 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useRef } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
 
   console.log("State rerendered");
 
-  const result = useMemo(() => {
+  const result = () => {
     let sum = 0;
     for (let i = 0; i < 123123123; i++) {
       sum += i;
     }
     return sum;
-  }, []);
+  };
 
   return (
     <div>
-      <h1>Expensive Calculation Result: {result}</h1>
+      <h1>Expensive Calculation Result: {result()}</h1>
       <p>Count: {count}</p>
       <button onClick={() => setCount(count + 1)}>Increment Count</button>
+      <Input />
+    </div>
+  );
+}
+
+function Input() {
+  const usernameRef = useRef("");
+  console.log("Input Component Rerendered");
+
+  return (
+    <div className="welcome-container">
+      <h1>Welcome to Our App</h1>
+      <div className="user-input">
+        <label htmlFor="username">Enter your name:</label>
+        <input
+          id="username"
+          type="text"
+          defaultValue={usernameRef.current}
+          onChange={(e) => (usernameRef.current = e.target.value)}
+          placeholder="Type your name here"
+        />
+      </div>
     </div>
   );
 }
