dc19b4bfbd02990254c3940967a18d8dcb100058
cHANGTEEZY <jagatgurung30@gmail.com>
2025-04-09 15:10:52 +0545

    added projects

--- Rerendering/ReRendering Bad Example/src/App.jsx
+++ Rerendering/ReRendering Bad Example/src/App.jsx
@@ -5,17 +5,17 @@ function App() {
 
   console.log("State rerendered");
 
-  const result = () => {
+  const result = useMemo(() => {
     let sum = 0;
     for (let i = 0; i < 123123123; i++) {
       sum += i;
     }
     return sum;
-  };
+  },[]);
 
   return (
     <div>
-      <h1>Expensive Calculation Result: {result()}</h1>
+      <h1>Expensive Calculation Result: {result}</h1>
       <p>Count: {count}</p>
       <button onClick={() => setCount(count + 1)}>Increment Count</button>
       <Input />
